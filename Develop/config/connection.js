216d09b7ff8f590edd2f5c60c212f524ec83864f
ausdr <austin.drury@du.edu>
2023-11-29 17:32:05 -0500

    Finalizing v4.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,26 +1,49 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   // Heroku deployment
+//   sequelize = new Sequelize(process.env.JAWSDB_URL, {
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, // You may need this depending on your database setup
+//       },
+//     },
+//   });
+// } else {
+//   // Local development
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//     port: 3306,
+//   });
+// }
+
+// module.exports = sequelize;
+
+
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  // Heroku deployment
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // You may need this depending on your database setup
-      },
-    },
-  });
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // Local development
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST || "localhost",
+      dialect: "mysql",
+      port: process.env.DB_PORT || 3306,
+    }
+  );
 }
-
 module.exports = sequelize;
+
