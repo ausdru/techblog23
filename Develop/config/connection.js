39567164bab4e98dbcd0ce9ddc5a78ff0a6385db
ausdr <austin.drury@du.edu>
2023-11-29 13:09:09 -0500

    Finalizing, preparing for deployment.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,23 +1,26 @@
-// Insert Notes Here
-
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// Insert Notes Here
+let sequelize;
 
-let sequelize; 
-
-process.env.JAWSDB_URL ? sequelize = new Sequelize(process.env.JAWSDB_URL)
-    : sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: '127.0.0.1',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
+if (process.env.JAWSDB_URL) {
+  // Heroku deployment
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // You may need this depending on your database setup
+      },
+    },
+  });
+} else {
+  // Local development
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
